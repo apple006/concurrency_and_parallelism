91b9a88b9cfe7914a62f07617d1f05faae577add
root <zhaodedong91@gmail.com>
2017-04-04 15:22:21 +0800

    add dining_philosophers

---
+++ dining_philosophers/Chopstick.java
@@ -0,0 +1,8 @@
+/**
+* 哲学家用的筷子类
+*/
+class Chopstick {
+	private int id;
+	public Chopstick(int id) { this.id = id; }
+  public int getId() { return id; }
+}
---
+++ dining_philosophers/DiningPhilosophers.java
@@ -0,0 +1,20 @@
+/**
+* 哲学家进餐的主类
+*/
+
+public class DiningPhilosophers {
+
+  public static void main(String[] args) throws InterruptedException {
+    Philosopher[] philosophers = new Philosopher[5];
+    Chopstick[] chopsticks = new Chopstick[5];
+
+    for (int i = 0; i < 5; ++i)
+      chopsticks[i] = new Chopstick(i);
+    for (int i = 0; i < 5; ++i) {
+      philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i + 1) % 5]);
+      philosophers[i].start();
+    }
+    for (int i = 0; i < 5; ++i)
+      philosophers[i].join();
+  }
+}
